038b0baf24996bde0f5cfe42a70a9a5096039483
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2023-08-07 14:34:41 +0900

    [Gold V] Title: 탑, Time: 3192 ms, Memory: 132568 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(in.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+				
+		int[] answer = new int[N];
+		Stack<Integer> stack = new Stack<>();
+		
+		for (int i = N-1; i >= 0; i--) {
+			if (stack.isEmpty()) stack.push(i);
+			else {
+				if (arr[i] < arr[stack.peek()]) {
+					stack.push(i);
+				} else {	
+					while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+						int idx = stack.pop();
+						answer[idx] = i+1;
+					}
+					stack.push(i);
+				}
+			}
+		}
+				
+		for (int i = 0; i < answer.length; i++) {
+			System.out.print(answer[i] + " ");
+		}
+	}
+
+}
