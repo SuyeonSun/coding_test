d91b2ba56895621cafd138aadebfb8ea001299be
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2023-08-25 13:16:03 +0900

    [Gold III] Title: 줄 세우기, Time: 432 ms, Memory: 46688 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,45 +4,58 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = in.readLine().split(" ");
-        int N = Integer.parseInt(str[0]);
-        int M = Integer.parseInt(str[1]);
 
-        int[] inDegree = new int[N+1];
-        ArrayList<Integer>[] list = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < M; i++) {
-            String[] input = in.readLine().split(" ");
-            int x = Integer.parseInt(input[0]);
-            int y = Integer.parseInt(input[1]);
-            list[x].add(y);
-            inDegree[y]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<Integer>();
-        for(int i=1; i<=N; i++){
-            if(inDegree[i]==0){
-                queue.add(i);
-            }
-        }
-        while(!queue.isEmpty()){
-            System.out.println(queue.peek());
-            int current = queue.poll();
-
-            //자신이 가르키고 있는 좌표들을 방문하여 indegree값을 -1 해주고 만약 0이라면 큐에 넣어준다.
-            for(int i=0; i<list[current].size(); i++){
-                int next = list[current].get(i);
-                inDegree[next]--;
-                if(inDegree[next]==0){
-                    queue.add(next);
-                }
-            }
-        }
-    }
+	static int N, M;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static int[] student;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		student = new int[N+1];
+		
+		for(int i = 0; i<=N; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		for(int i = 0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			graph.get(v).add(w);
+			student[w]++;
+		}
+		
+		bfs();
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i = 1; i<=N; i++) {
+			if(student[i] == 0) {
+				queue.offer(i);
+				sb.append(i).append(' ');
+			}
+		}
+		
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+			
+			for(int i : graph.get(cur)) {
+				student[i]--;
+				if(student[i] == 0) {
+					queue.offer(i);
+					sb.append(i).append(' ');
+				}
+			}
+		}
+	}
 }
