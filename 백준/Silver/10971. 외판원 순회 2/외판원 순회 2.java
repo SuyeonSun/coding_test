319ebc1e9ed3ccb18094b83e3df7b510154030c5
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2023-08-30 15:56:50 +0900

    [Silver II] Title: 외판원 순회 2, Time: 272 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,62 +1,52 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static boolean[] isVisited;
-	static int[] numbers;
-	static int[][] arr;
-	static int min = Integer.MAX_VALUE;
-	
-	public static void permutation(int cnt) {
-		if (cnt == N) {
-			ArrayList<Integer> route = new ArrayList<>();
-			for (int i = 0; i < N; i++) {
-				route.add(numbers[i]);
-			}
-			route.add(numbers[0]); // 다시 돌아오는 경로 추가
-			// System.out.println(route);
-			
-			// 여행 비용 계산
-			int cost = 0;
-			for (int i = 0; i < route.size()-1; i++) {
-				if (arr[route.get(i)][route.get(i+1)] == 0) return;
-				cost += arr[route.get(i)][route.get(i+1)];
-			}
-			
-			// 최소 여행 비용 업데이트
-			if (cost < min) min = cost;
-			
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			if (isVisited[i]) continue;
-			numbers[cnt] = i;
-			isVisited[i] = true;
-			permutation(cnt+1);
-			isVisited[i] = false;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(in.readLine());
-		arr = new int[N][N];
+
+	static int N, ans = Integer.MAX_VALUE;
+	static int cost[][];
+	static boolean visited[];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		cost = new int[N][N];
+		visited = new boolean[N];
 		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<N; j++) {
+				cost[i][j] = Integer.parseInt(st.nextToken());
+				if(cost[i][j] == 0) cost[i][j] = Integer.MAX_VALUE;
 			}
 		}
 		
-		isVisited = new boolean[N];
-		numbers = new int[N];
-		permutation(0);
-		System.out.println(min);
+		for(int i = 0; i<N; i++) {
+			visited[i] = true;
+			dfs(i, 0, 1, i);
+			visited[i] = false;
+		}
+		System.out.println(ans);
+	}
+
+	static void dfs(int from, int c, int depth, int start) {
+		if(depth == N && from != N-1) return;
+		if(from == N-1) {
+			if(depth == N) {
+				if(cost[from][start] != Integer.MAX_VALUE) {
+					ans = Math.min(ans, c+cost[from][start]);
+				}
+				return;
+			}
+		}
+		
+		for(int i =0; i<N; i++) {
+			if(visited[i]) continue;
+			if(cost[from][i] != Integer.MAX_VALUE) {
+				visited[i] = true;
+				dfs(i, c+cost[from][i], depth+1, start);
+				visited[i] = false;
+			}
+		}
 	}
 }
