9348b3f22cb42fdcc5c4ad086f56b267f02d6152
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2023-09-06 09:25:35 +0900

    [Gold V] Title: 잠수함식별, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Gold/2671. 잠수함식별/잠수함식별.java
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		if(str.matches("(10(0)+1+|01)+")) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		if(str.matches("(100+1+|01)+")) {
 			System.out.println("SUBMARINE");
 		}
 		else {
