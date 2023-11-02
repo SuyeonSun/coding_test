59a5762e90f66caf8d1df72d509534a465aea04b
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2023-11-02 16:40:00 +0900

    [Silver I] Title: 회의실 배정, Time: 516 ms, Memory: 41892 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,38 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(in.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            pq.add(new int[] {s, e});
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(in.readLine());
-		int[][] time = new int[N][2];
-		for (int i = 0; i < N; i++) {
-			String[] input = in.readLine().split(" ");
-			time[i][0] = Integer.parseInt(input[0]);
-			time[i][1] = Integer.parseInt(input[1]);
-		}
-		
-		Arrays.sort(time, (o1, o2) -> {
-			if (o1[1] == o2[1]) { // 종료 시간이 같다면
-				return o1[0] - o2[0]; // 시작 시간이 작은 값 리턴
-			}
-			return o1[1]- o2[1]; // 종료 시간 오름차순
-		});
-		
-		int cnt = 0;
-		int end = 0;
-		
-		for (int i = 0; i < N; i++) {
-			if (end <= time[i][0]) { // 현재 끝난 시간 보다 다음 시작 시간이 더 뒤라면
-				end = time[i][1];
-				cnt++;
-			}
-		}
-		
-		System.out.println(cnt);
-	}
+        int cnt = 0;
+        int end = 0;
 
+        for (int i = 0; i < N; i++) {
+            int[] poll = pq.poll();
+            if (end <= poll[0]) {
+                end = poll[1];
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
