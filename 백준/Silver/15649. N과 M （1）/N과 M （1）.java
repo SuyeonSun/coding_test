af72a7c9a5cd39b5be179ebeb87c5a481c9a973e
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2023-11-13 21:13:28 +0900

    [Silver III] Title: N과 M (1), Time: 2392 ms, Memory: 76132 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,39 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] arr;
-	static int[] numbers;
-	static boolean[] isSelected;
-	static int M;
-	
-	public static void permutation (int cnt) {
-		if (cnt == M) {
-			for (int i = 0; i < numbers.length; i++) {
-				System.out.print(numbers[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for (int i = 0; i < arr.length; i++) {
-			if (!isSelected[i]) {
-				isSelected[i] = true;
-				numbers[cnt] = arr[i]; 
-				permutation (cnt + 1);
-				isSelected[i] = false;
-			}
-		}
-	}
-	
-	public static void main (String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		isSelected = new boolean[N];
-		numbers = new int[M];		
-		for (int i = 0; i < N; i++) arr[i] = i + 1;
-		permutation(0);
-	}
+    static int N;
+    static int M;
+    static int[] numbers;
+    static boolean[] isVisited;
+
+    public static void combination(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(numbers[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (isVisited[i]) continue;
+            numbers[cnt] = i+1;
+            isVisited[i] = true;
+            combination(cnt+1);
+            isVisited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[M];
+        isVisited = new boolean[N];
+        combination(0);
+    }
 }
