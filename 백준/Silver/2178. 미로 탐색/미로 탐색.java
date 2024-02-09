1840c58ad5e2a35ea03b9e1eed66232eb905d640
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-02-09 16:41:45 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14740 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,55 +6,57 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static boolean[][] visited;
-	public static int[][] arr;
-	public static Queue<int[]> queue = new LinkedList<>();
-	public static int cnt = 1;
-	public static int[] dx = {0, 0, -1, 1};
-	public static int[] dy = {1, -1, 0, 0};
-	public static int N;
-	public static int M;
+	private static int N;
+	private static int M;
+	private static int[][] arr;
+	private static boolean[][] isVisited;
+	private static final int[] dx = {-1, 1, 0, 0};
+	private static final int[] dy = {0, 0, -1, 1};
 
-	public static int bfs(int i, int j, int c) {	
+	public static int bfs(int i, int j, int c) {
+		Queue<int[]> queue = new LinkedList<>();
 		queue.add(new int[] {i, j, c});
-		visited[i][j] = true;
-		
-		while(!queue.isEmpty()) {
+		isVisited[i][j] = true;
+		int cnt = 0;
+
+		while (!queue.isEmpty()) {
 			int[] poll = queue.poll();
 			int x = poll[0];
 			int y = poll[1];
-			int cnt = poll[2];
-			if (x == N-1 && y == M-1) {
-				return cnt;
-			}
+			int z = poll[2];
+
+			if (x == N - 1 && y == M - 1) return z;
+
 			for (int d = 0; d < 4; d++) {
 				int nx = x + dx[d];
 				int ny = y + dy[d];
-				if (nx >= N || ny >= M || nx < 0 || ny < 0) continue;
-				if (!visited[nx][ny] && arr[nx][ny] == 1) {
-					queue.add(new int[] {nx, ny, cnt+1});
-					visited[nx][ny] = true;
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+				if (arr[nx][ny] == 1 && !isVisited[nx][ny]) {
+					queue.add(new int[] {nx, ny, z + 1});
+					isVisited[nx][ny] =  true;
 				}
 			}
 		}
-		return cnt;
+
+		return c;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		// 입력
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N][M];
 		arr = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			String[] input = in.readLine().split("");
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(input[j]);
+		isVisited = new boolean[N][M];
+
+		for (int n = 0; n < N; n++) {
+			String str = in.readLine();
+			for (int m = 0; m < M; m++) {
+				arr[n][m] = str.charAt(m) - '0';
 			}
 		}
-		
-		System.out.println(bfs(0, 0, 1));
+
+		int answer = bfs(0, 0, 1);
+		System.out.println(answer);
 	}
 }
