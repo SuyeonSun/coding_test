33e8aa6aa250bfc4e12daf58f3791a4b146c2561
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-02-09 21:08:29 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -9,36 +9,33 @@ public class Main {
 	private static int N;
 	private static int M;
 	private static int[][] arr;
-	private static boolean[][] isVisited;
+	private static int[][] dist;
 	private static final int[] dx = {-1, 1, 0, 0};
 	private static final int[] dy = {0, 0, -1, 1};
 
-	public static int bfs(int i, int j, int c) {
+	public static void bfs(int i, int j) {
 		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] {i, j, c});
-		isVisited[i][j] = true;
-		int cnt = 0;
+		queue.add(new int[] {i, j});
+		dist[i][j] = 0; // 첫 시작이기 때문에 거리는 0이다.
 
 		while (!queue.isEmpty()) {
 			int[] poll = queue.poll();
 			int x = poll[0];
 			int y = poll[1];
-			int z = poll[2];
-
-			if (x == N - 1 && y == M - 1) return z;
 
 			for (int d = 0; d < 4; d++) {
 				int nx = x + dx[d];
 				int ny = y + dy[d];
+				// 벽에 부딪히거나 범위를 넘어가는 경우
 				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-				if (arr[nx][ny] == 1 && !isVisited[nx][ny]) {
-					queue.add(new int[] {nx, ny, z + 1});
-					isVisited[nx][ny] =  true;
-				}
+				// 길이 아니거나 이미 방문을 한 경우
+				if (arr[nx][ny] == 0 || dist[nx][ny] != -1) continue;
+
+				queue.add(new int[]{nx, ny});
+				dist[nx][ny] = dist[x][y] + 1;
 			}
 		}
-
-		return c;
+		System.out.println(dist[N-1][M-1] + 1);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -47,16 +44,16 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N][M];
-		isVisited = new boolean[N][M];
+		dist = new int[N][M];
 
 		for (int n = 0; n < N; n++) {
 			String str = in.readLine();
 			for (int m = 0; m < M; m++) {
 				arr[n][m] = str.charAt(m) - '0';
+				dist[n][m] = -1;
 			}
 		}
 
-		int answer = bfs(0, 0, 1);
-		System.out.println(answer);
+		bfs(0, 0);
 	}
 }
