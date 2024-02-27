ec0fc5d79d53444e13df8878cee8333def84db65
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-02-27 13:51:50 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 17976 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,17 +6,32 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
-		int[] dp = new int[N+1];
-		dp[0] = dp[1] = 0;
+
+		int[] di = new int[N+1];
+		di[0] = 0;
+		di[1] = 0;
+
+		// 연산을 사용하는 횟수의 최솟값 -> Math.min(a, b) 사용
+
+		// di[1] = 0
+		// di[2] = 1
+
+		// di[3] = 1
+		// 3 - 1 % 2
+		// 3 - 2
+		// 3 - 1 - 1
+		// 3 % 3
+
 		for (int i = 2; i <= N; i++) {
-			dp[i] = dp[i-1] + 1; // -1은 무조건 적용
-			if (i % 2 == 0) { 
-				dp[i] = Math.min(dp[i], dp[i/2] + 1);
-			}
+			di[i] = di[i-1] + 1; // 전 값 보다는 무조건 1 더해지기 때문에
 			if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/3] + 1);
+				di[i] = Math.min(di[i], di[i/3]+1);
+			}
+			if (i % 2 == 0) {
+				di[i] = Math.min(di[i], di[i/2]+1);
 			}
 		}
-		System.out.println(dp[N]);
+
+		System.out.println(di[N]);
 	}
 }
