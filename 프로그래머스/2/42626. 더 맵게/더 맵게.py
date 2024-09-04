a2f95803024beb149c5b27747065bbad9275a310
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-04 18:36:53 +0900

    [level 2] Title: 더 맵게, Time: 1636.98 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,22 +3,17 @@ import heapq
 def solution(scoville, K):
     answer = 0
     
-    # 힙으로 변환(최소 힙)
     heapq.heapify(scoville)
     
     while scoville:
         if scoville[0] >= K:
             return answer
-                
         first = heapq.heappop(scoville)
-        
-        # 두번째 원소를 꺼낼 수 없다면
         if not scoville:
             return -1
-            
         second = heapq.heappop(scoville)
-        mixed_scoville = first + (second * 2)
-        heapq.heappush(scoville, mixed_scoville)
+        new = first + (second*2)
+        heapq.heappush(scoville, new)
         answer += 1
-        
+    
     return answer
