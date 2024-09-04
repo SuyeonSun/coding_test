0559e03b79b0dc3327805a3d76d05db303b6eec4
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-04 09:33:27 +0900

    [level 2] Title: 땅따먹기, Time: 161.18 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,10 @@
+def solution(land):
+    # land 배열을 갱신하며 최대값을 누적해가면서 계산
+    for i in range(1, len(land)):
+        land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
+        land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
+        land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
+        land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])
+    
+    # 마지막 행에서 최대값을 반환
+    return max(land[-1])
