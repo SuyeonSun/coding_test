2994eb040e68fdd8464f2bd34c31b58c99147356
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-06 12:12:20 +0900

    [level 2] Title: 소수 찾기, Time: 4.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 23.50 ms
+메모리: 10.3 MB, 시간: 4.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 06일 11:54:40
+2024년 09월 06일 12:12:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -2,23 +2,27 @@ from itertools import permutations
 import math
 
 def solution(numbers):
-    answer = []
+    primes = set()
+    num_set = set()
     
     def is_prime(n):
-        if n == 0 or n == 1: # 소수는 1보다 큰 정수
+        if n <= 1: # 소수는 1보다 큰 정수
             return False
-        else:
-            for i in range(2, int(math.sqrt(n))+1):
-                if n % i == 0:
-                    return False
+        if n == 2:
             return True
+        if n % 2 == 0:
+            return False
+        for i in range(3, int(math.sqrt(n)) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
     
-    for i in range(1, len(numbers)+1):
-        arr = list(permutations(numbers, i)) # permutations(numbers, i) 이것만 하면, ('1', '7') 이렇게 튜플 형태로 반환되기 때문이다.
-        for j in range(len(arr)):
-            num = int(''.join(map(str,arr[j])))
-            if is_prime(num):
-                answer.append(num)
-                
-    answer=list(set(answer))
-    return len(answer)
+    for i in range(1, len(numbers) + 1):
+        for perm in permutations(numbers, i):
+            num = int(''.join(perm))
+            if num not in num_set:
+                num_set.add(num)
+                if is_prime(num):
+                    primes.add(num)
+                    
+    return len(primes)
