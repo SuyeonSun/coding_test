6fc6f9fd9f04b44715ff23ff9870de30c2223036
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-08 21:09:28 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,30 +1,29 @@
 def solution(n, words):
     answer = []
-
-    # [가장 먼저 탈락하는 사람의 번호, 그 사람이 자신의 몇 번째 차례에 탈락하는지]
-    # [0, 0]
     
-    # 끝 글자가 맞지 않거나
-    # 같은 글자를 반복하거나
     
-    said_words = [words[0]]
-    turn = 2
+    # % 번째
+    # // 차례
+    said_words = set()
     
-    for i in range(1, len(words)):
-        if (turn > n):
-            turn = 1
-        if (words[i] not in said_words and said_words[-1][-1] == words[i][0]):
-            said_words.append(words[i])
-            turn += 1
-        else:
-            print(turn)
-            if ((i+1) % n != 0):
-                answer = [turn, (i+1) // n + 1]
-            else:
-                answer = [turn, (i+1) // n]
-            break   
+    def is_valid(idx):
+        word = words[idx]
+        if word in said_words:
+            return False
+        if words[idx - 1][-1] != word[0]:
+            return False
+        said_words.add(words[idx])
+        return True    
     
-    if (len(answer) == 0):
+    for i in range(len(words)):
+        if i == 0:
+            said_words.add(words[i])
+            continue
+        if is_valid(i) == False:
+            answer = [i % n + 1, i // n + 1]
+            return answer
+    
+    if len(answer) == 0:
         answer = [0, 0]
         
     return answer
