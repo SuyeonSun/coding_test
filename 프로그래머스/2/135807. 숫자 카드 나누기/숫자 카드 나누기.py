990facf55f4cea4982a147522af56993dec9debe
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-11 09:50:31 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 213.80 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -0,0 +1,25 @@
+import math
+
+def solution(arrayA, arrayB):
+    answer = 0
+    
+    # 최대공약수 math.gcd
+    gcd_a = arrayA[0]
+    gcd_b = arrayB[0]
+
+    for i in range(1, len(arrayA)):
+        gcd_a = math.gcd(gcd_a,arrayA[i])
+        gcd_b = math.gcd(gcd_b,arrayB[i])
+    
+    def is_valid(gcd_value, opponent_array):
+        for i in opponent_array:
+            if i % gcd_value == 0:
+                return False
+        return True
+    
+    if is_valid(gcd_a, arrayB):
+        answer = max(answer, gcd_a)
+    if is_valid(gcd_b, arrayA):
+        answer = max(answer, gcd_b)
+    
+    return answer
