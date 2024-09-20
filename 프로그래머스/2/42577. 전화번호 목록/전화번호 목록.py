621bed88acad5f70f90e8a860b7645cb2452d6b8
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-20 10:37:28 +0900

    [level 2] Title: 전화번호 목록, Time: 94.73 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,16 +1,10 @@
 def solution(phone_book):
     answer = True
     
-    hash_map = {}
+    phone_book.sort()
     
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1 # 같은 전화번호는 없음
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
     
-    for phone_number in phone_book:
-        temp = ""
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                answer = False
-                
     return answer
