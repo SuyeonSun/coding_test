c7c601dc94396b947f3de16f49d4bf8433ab4d63
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-22 19:02:39 +0900

    [level 2] Title: 숫자 변환하기, Time: 491.63 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,19 +1,22 @@
 from collections import deque
 
 def solution(x, y, n):
-    visited = [0] * 1000001
+    answer = 0
+    
+    visited = [False] * 10000001
     
     q = deque()
     q.append((x, 0))
-    visited[x] = 1
+    visited[x] = True
     
     while q:
-        num, cnt = q.popleft() # bfs의 핵심
+        num, cnt = q.popleft()
         if num == y:
             return cnt
-        for next_num in (num + n, num * 2, num * 3):
+        next_nums = [num + n, num * 2, num * 3]
+        for next_num in next_nums:
             if next_num <= y and not visited[next_num]:
-                visited[next_num] = 1
+                visited[next_num] = True
                 q.append((next_num, cnt + 1))
     
     return -1
