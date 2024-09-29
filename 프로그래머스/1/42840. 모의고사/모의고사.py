69cc554c3bc48c1b2483fc9eda29609d159da004
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-29 14:45:00 +0900

    [level 1] Title: 모의고사, Time: 3.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -7,16 +7,16 @@ def solution(answers):
     student2 = [2,1,2,3,2,4,2,5]
     student3 = [3,3,1,1,2,2,4,4,5,5]
     
-    for i in range(len(answers)) :
+    for i in range(len(answers)):
         if answers[i] == student1[i%5] :
             score[0] += 1
         if answers[i] == student2[i%8] :
             score[1] += 1
         if answers[i] == student3[i%10] :
             score[2] += 1
-        
-    for idx, num in enumerate(score) :
-        if num == max(score) :
-            answer.append(idx +1)
+    
+    for i in range(len(score)):
+        if score[i] == max(score):
+            answer.append(i + 1)
     
     return answer
