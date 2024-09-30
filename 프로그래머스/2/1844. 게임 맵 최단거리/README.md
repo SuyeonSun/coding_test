398b8b11e593fa385333767dc196196f3edce08c
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-30 20:17:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.69 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 14.16 ms
+메모리: 10.5 MB, 시간: 14.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 08:33:45
+2024년 09월 30일 20:17:32
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,32 @@
 from collections import deque
 
 def solution(maps):
+    global answer
     answer = 0
     
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
-    def bfs(i, j, cnt, maps): # isVisited를 쓰면 안되는 문제: 벽이 있기 때문에
-        queue = deque()
-        queue.append([i, j, cnt])
+    visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    
+    def bfs():
+        queue = deque([])
+        queue.append([0, 0, 1])
+        visited[0][0] = True
         while queue:
-            x, y, cnt = queue.popleft()
-            
+            x, y, c = queue.popleft()
             if x == len(maps) -1 and y == len(maps[0]) -1:
-                return cnt
-            
+                return c
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
                 if nx < 0 or ny < 0 or nx >= len(maps) or ny >= len(maps[0]):
                     continue
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = -1
-                    queue.append([nx, ny, cnt+1])
+                if not visited[nx][ny] and maps[nx][ny] == 1:
+                    queue.append([nx, ny, c + 1])
+                    visited[nx][ny] = True
         return -1
-            
-    answer = bfs(0, 0, 1, maps) # 시작점을 1로 시작
+    
+    answer = bfs()
     
     return answer
