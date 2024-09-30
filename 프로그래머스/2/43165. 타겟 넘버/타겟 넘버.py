398b304011755e8e90ace75a3227404326b7143b
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-30 16:39:40 +0900

    [level 2] Title: 타겟 넘버, Time: 414.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,18 +2,16 @@ def solution(numbers, target):
     global answer
     answer = 0
     
-    def dfs(numbers, target, total, index):
-        global answer
-
-        if index > len(numbers) - 1:
+    def dfs(idx, cnt, total):
+        global answer 
+        
+        if cnt == len(numbers):
             if total == target:
                 answer += 1
             return
-        
-        dfs(numbers, target, total + numbers[index], index + 1)
-        dfs(numbers, target, total - numbers[index], index + 1)
-        
-    dfs(numbers, target, 0, 0)
+        dfs(idx + 1, cnt + 1, total + numbers[idx])
+        dfs(idx + 1, cnt + 1, total - numbers[idx])
     
+    dfs(0, 0, 0)
     
     return answer
