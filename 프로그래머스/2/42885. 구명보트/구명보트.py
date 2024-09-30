66f1d5f80cd6e69320b062ea2d9cc5b567906e20
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-09-30 11:01:53 +0900

    [level 2] Title: 구명보트, Time: 9.48 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,16 +2,13 @@ from collections import deque
 
 def solution(people, limit):
     answer = 0
-
-    people = deque(sorted(people)) # deque 사용 시에는, sort() 메서드는 사용할 수 없고, sorted()만 사용 가능
     
-    while True:
-        if not people:  # people이 비어있다면 루프를 종료
-            break
-            
-        person = people.pop()
-        if (len(people) > 0 and person + people[0] <= limit):
-            people.popleft()
+    people = deque(sorted(people, reverse = True))
+    
+    while people:
+        person = people.popleft()
+        if people and person + people[-1] <= limit:
+            people.pop()
         answer += 1
     
     return answer
