82626cf815f2bea2cf88f6b765a26b1e167e611d
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-10-01 09:30:54 +0900

    [level 2] Title: 주식가격, Time: 32.69 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,19 @@
 def solution(prices):
-    answer = [0] * len(prices)
+    answer = []
+    for i in range(len(prices)):
+        answer.append(0)
     
-    stack = []
+    stack = [] # 가격이 떨어지지 않은 주식만 남도록 한다.
     
     for i in range(len(prices)):
-        while stack and prices[i] < prices[stack[-1]]:
-            idx = stack.pop()
-            answer[idx] = i - idx
-        stack.append(i)
+            while stack and prices[stack[-1]] > prices[i]:
+                    pop = stack.pop()
+                    diff = i - pop
+                    answer[pop] = diff
+            stack.append(i)
     
     while stack:
         idx = stack.pop()
-        answer[idx] = len(prices) - 1 - idx
+        answer[idx] = len(prices) - idx - 1
     
     return answer
