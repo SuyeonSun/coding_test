9afe5c23ec5213baa0756761c03c392b45280a67
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-10-01 10:02:01 +0900

    [level 2] Title: 가장 큰 수, Time: 2016.23 ms, Memory: 25 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,23 +1,25 @@
 import functools
 
-def comparator(a,b):
-    t1 = a+b # 두 문자열을 붙인 결과
-    t2 = b+a # 반대로 붙인 결과
-    
-    if int(t1) > int(t2):
+def comparator(a, b):
+    n1 = a + b
+    n2 = b + a
+    if int(n1) > int(n2):
         return 1
-    elif int(t1) == int(t2):
+    elif int(n1) == int(n2):
         return 0
     else:
         return -1
 
 def solution(numbers):
     answer = ''
+    max_val = 0
     
     n = []
-    for number in numbers:
-        n.append(str(number))
+    for num in numbers:
+        n.append(str(num))
+    
     n = sorted(n, key = functools.cmp_to_key(comparator), reverse = True)
+    
     answer = str(int(''.join(n)))
     
     return answer
