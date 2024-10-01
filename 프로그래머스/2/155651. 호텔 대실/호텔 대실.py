9ffe6a274b96eed28d2e128aa744fe3ebd6aa546
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-10-01 10:47:00 +0900

    [level 2] Title: 호텔 대실, Time: 2.72 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,28 +1,25 @@
 import heapq
 
-def time_to_minute(time_str):
-    hours, minutes = map(int, time_str.split(":"))
-    return hours * 60 + minutes
-    
 def solution(book_time):
     answer = 0
     
-    events = []
+    def hour_to_min(time_str):
+        hours, minutes = map(int, time_str.split(":"))
+        return hours * 60 + minutes
     
-    for start, end in book_time:
-        start_time = time_to_minute(start)
-        end_time = time_to_minute(end) + 10
-        events.append([start_time, end_time])
+    events = []
+    for s, e in book_time:
+        start = hour_to_min(s)
+        end = hour_to_min(e) + 10
+        events.append([start, end])
     
     events.sort()
     
     heap = []
-    
     for start, end in events:
-        if heap and heap[0] <= start:   
+        if heap and heap[0] <= start:
             heapq.heappop(heap)
         heapq.heappush(heap, end)
     
     answer = len(heap)
-    
     return answer
