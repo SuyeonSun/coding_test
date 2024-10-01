41739df06e13fe2c384a122f24466603b3302a4b
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-10-01 09:20:00 +0900

    [level 2] Title: 더 맵게, Time: 1576.58 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
 
 ### 성능 요약
 
-메모리: 51.8 MB, 시간: 1591.01 ms
+메모리: 51.7 MB, 시간: 1576.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 09:12:03
+2024년 10월 01일 09:19:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,7 +3,6 @@ import heapq
 def solution(scoville, K):
     answer = 0
     
-    # 가장 작은 순서대로 pop이 필요하기 때문에 min heap 사용
     heapq.heapify(scoville)
     
     while scoville:
@@ -13,9 +12,9 @@ def solution(scoville, K):
         if not scoville:
             return -1
         second = heapq.heappop(scoville)
-        
         mixed = first + (second * 2)
         heapq.heappush(scoville, mixed)
         answer += 1
         
+    
     return answer
