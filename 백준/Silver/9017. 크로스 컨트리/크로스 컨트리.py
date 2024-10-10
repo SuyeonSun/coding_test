fd70418971f9732e44b78dd149bc70462db84e32
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-10-10 11:43:04 +0900

    [Silver III] Title: 크로스 컨트리, Time: 60 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,19 +1,21 @@
-from collections import defaultdict
 import sys
+from collections import Counter
 input = sys.stdin.readline
 
 T = int(input())
-for _ in range(T) :
+for _ in range(T):
   N = int(input())
-  num_list = list(map(int, input().split()))
-  num_cnt = defaultdict(int)
-  num_dict = defaultdict(list)
+  teams = list(map(int, input().split()))
+  counter = Counter(teams)
+  scores = {}
 
-  for num in num_list :
-    num_cnt[num] += 1
-  num_list = [ x for x in num_list if num_cnt[x] == 6 ]
-  for idx, num in enumerate(num_list) :
-    num_dict[num].append(idx)
-  result = list(num_dict.keys())
-  result.sort( key = lambda x : (sum(num_dict[x][:4]), num_dict[x][4], num_dict[x][5]))
-  print(result[0])
+  rank = 1
+  for i in range(N):
+    if counter[teams[i]] == 6:
+      if teams[i] in scores:
+        scores[teams[i]].append(rank)
+      else:
+        scores[teams[i]] = [rank]
+      rank += 1
+  
+  print(sorted(scores, key = lambda x:(sum(scores[x][0:4]), scores[x][4]))[0])
