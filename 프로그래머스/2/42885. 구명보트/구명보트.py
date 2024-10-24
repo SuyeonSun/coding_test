cba68a3665170777f92c3d46f5d57f36a9902810
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-10-24 09:39:01 +0900

    [level 2] Title: 구명보트, Time: 14.05 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -3,12 +3,16 @@ from collections import deque
 def solution(people, limit):
     answer = 0
     
-    people = deque(sorted(people, reverse = True))
+    people = deque(sorted(people))
     
-    while people:
-        person = people.popleft()
-        if people and person + people[-1] <= limit:
-            people.pop()
+    while True:
+        if not people:
+            break
+        person = people.pop()
+        if len(people) > 0 and people[0] + person <= limit:
+            people.popleft()
         answer += 1
+    
+    
     
     return answer
