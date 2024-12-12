1340d15cb6bb241bfdb67d2753399ee20791e334
Suyeon Sun <77823761+SuyeonSun@users.noreply.github.com>
2024-12-12 11:16:47 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+arr = [0] * M
+
+def dfs(index, depth, arr):
+    if depth == M:
+        print(*arr)
+        return
+    for i in range(index, N):
+        arr[depth] = i + 1
+        dfs(i, depth + 1, arr)
+
+dfs(0, 0, arr)
