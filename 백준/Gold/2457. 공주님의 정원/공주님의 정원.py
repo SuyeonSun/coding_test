12e560f4469de18efdeaf80757f3be2c5ff06e77
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2024-12-18 12:12:11 +0900

    [Gold III] Title: 공주님의 정원, Time: 256 ms, Memory: 64208 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -1,48 +1,39 @@
-n = int(input())
-date = []
-# 편의를 위해 100을 곱해 날짜 형식으로 바꿈
-for i in range(n):
-    temp = list(map(int, input().split())) # 1 1 5 31
-    date.append([temp[0] * 100 + temp[1], temp[2] * 100 + temp[3]])
-
-# 꽃이 피고 지는 날짜를 오름차순으로 정렬
-date.sort(key = lambda x: (x[0], x[1])) 
-# [[101, 531], [101, 630], [515, 831], [610, 1210]]
-
-# 선택한 꽃의 개수
-cnt = 0
-# 제일 늦게 지는 꽃을 비교
-end = 0
-# 마지막 꽃이 지는 날
-target = 301
-
-# 모든 꽃이 없어질 때까지 반복하여 꽃을 비교
-while date:
-    # 마지막 꽃이 지는 날이 12월1일보다 크거나 같을때
-    # 마지막 꽃이 지는날이 제일 빨리 피는 꽃보다 작을때
-    if target >= 1201 or target < date[0][0]:
-        break
+def princess_garden():
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
     
-    # 꽃 개수의 길이만큼 반복하여 구간별로 꽃을 비교
-    for _ in range(len(date)):
-        # 마지막 꽃의 지는 날이 제일 빨리 피는 꽃보다 크거나 같으면 제일 빨리 피는 꽃의 지는 날을 확인
-        if target >= date[0][0]:
-            # 그 꽃의 지는 날과 마지막으로 꽃의 지는 날을 비교한다.
-            # 그 꽃의 지는 날이 더 크면 더 오래 꽃을 볼 수 있기 때문에
-            # 그 꽃의 지는 날을 마지막 꽃의 지는 날로 변경
-            if end <= date[0][1]:
-                end = date[0][1]
-            
-            # 꽃을 확인하면 지운다.
-            date.remove(date[0])
-        # 꽃이 지는 날이 제일 빨리 피는 꽃보다 작으면 멈춘다.
-        else:
-            break
-    target = end
-    cnt += 1
+    n = int(data[0])  # 꽃의 개수
+    flowers = []
+    
+    for i in range(1, n + 1):
+        sm, sd, em, ed = map(int, data[i].split())
+        flowers.append((sm * 100 + sd, em * 100 + ed))
+    
+    # 꽃을 시작 날짜로 정렬, 같은 시작 날짜라면 끝나는 날짜로 내림차순 정렬
+    flowers.sort(key=lambda x: (x[0], -x[1]))
+    
+    current_end = 301  # 3월 1일부터 시작
+    max_end = 301
+    count = 0
+    i = 0
+    
+    while current_end <= 1130:
+        found = False
+        while i < n and flowers[i][0] <= current_end:
+            max_end = max(max_end, flowers[i][1])
+            i += 1
+            found = True
+        
+        if not found:
+            print(0)
+            return
+        
+        count += 1
+        current_end = max_end
+    
+    print(count)
 
-# 마지막 꽃의 지는 날이 12월 1일보다 작으면 11월 30일에는 피어있는 꽃이 없기때문에 0을 출력
-if target < 1201:
-    print(0)
-else:
-    print(cnt)
+# 실행 예시
+# 표준 입력이 제공될 경우 사용합니다.
+princess_garden()
