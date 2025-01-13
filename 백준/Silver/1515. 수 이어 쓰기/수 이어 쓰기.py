b72cd559bba2ccf2a7336aff5faa80fe3e674c78
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2025-01-13 10:17:33 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,12 +1,12 @@
-import sys
-nums = sys.stdin.readline().rstrip()
-n = 0
-idx = 0
+nums = input().rstrip()  # 찾을 숫자 입력
+current_number = 0  # 현재 자연수
+current_idx = 0  # nums의 현재 인덱스
+
 while True:
-    n += 1
-    for s in str(n):
-        if nums[idx] == s:
-            idx += 1
-            if idx >= len(nums):
-                print(n)
+    current_number += 1
+    for char in str(current_number):  # 현재 숫자를 문자열로 변환해 각 문자 비교
+        if nums[current_idx] == char:
+            current_idx += 1
+            if current_idx == len(nums):  # nums의 모든 문자를 찾으면 출력 후 종료
+                print(current_number)
                 exit()
