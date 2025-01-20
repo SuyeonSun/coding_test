4fd8c9fc78865277379af58247d9fabd14d19852
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2025-01-20 11:50:45 +0900

    [Silver III] Title: 두 수의 합, Time: 84 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,20 +1,21 @@
-import sys
-
-n = int(sys.stdin.readline())
-a = sorted(list(map(int, sys.stdin.readline().split(" "))))
-x = int(sys.stdin.readline())
+n = int(input())
+arr = list(map(int, input().split()))
+x = int(input())
 count = 0
 
+arr.sort()
+
 left = 0
-right = n-1
+right = n - 1
 
 while left < right:
-    temp = a[left] + a[right]
+    temp = arr[left] + arr[right]
     if temp == x:
         count += 1
         left += 1
     elif temp > x:
         right -= 1
-    else: left += 1
+    else:
+        left += 1
 
 print(count)
