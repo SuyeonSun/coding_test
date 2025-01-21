79d66b126af1983e0a92f159c1641be1049d81b4
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2025-01-21 11:23:26 +0900

    [level 2] Title: 올바른 괄호, Time: 10.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,17 +1,14 @@
-def solution(s):
-    answer = True
-    
+def solution(s):    
     stack = []
-    
     for i in s:
         if not stack:
             stack.append(i)
-        elif stack:
+        else:
             if stack[-1] == "(" and i == ")":
                 stack.pop()
             else:
                 stack.append(i)
-    
-    if stack:
+    if not stack:
+        return True
+    else:
         return False
-    return True
