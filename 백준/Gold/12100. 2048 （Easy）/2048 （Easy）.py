6c1782fe2476a7748ad0338be93136aee2b3793a
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2025-02-13 09:50:27 +0900

    [Gold I] Title: 2048 (Easy), Time: 304 ms, Memory: 33752 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,88 +1,110 @@
-import sys
-import copy
+import sys, copy
+N = int(input())
+pan = []
+ans = 0
 
-# 입력 받기
-N = int(sys.stdin.readline())
-board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+for _ in range(N):
+    pan.append([int(x) for x in sys.stdin.readline().rstrip().split()])
 
-# 보드 이동 함수
-def move(board, direction):
-    new_board = [[0] * N for _ in range(N)]
-    
-    if direction == 0:  # 위쪽 이동
-        for c in range(N):
-            queue, merged = [], [False] * N
-            for r in range(N):
-                if board[r][c] == 0:
-                    continue
-                if queue and queue[-1] == board[r][c] and not merged[len(queue)-1]:
-                    queue[-1] *= 2
-                    merged[len(queue)-1] = True  # 합쳐졌음을 표시
+def left(board):
+    for i in range(N):
+        cursor = 0
+        for j in range(1, N):
+            if board[i][j] != 0: # 0이 아닌 값이
+                tmp = board[i][j]
+                board[i][j] = 0 # 일단 비워질꺼니까 0으로 바꿈
+
+                if board[i][cursor] == 0: # 비어있으면
+                    board[i][cursor] = tmp # 옮긴다
+
+                elif board[i][cursor] == tmp: # 같으면
+                    board[i][cursor] *= 2 # 합친다
+                    cursor += 1
+                else: # 비어있지도 않고 다른 값일때
+                    cursor += 1 # pass
+                    board[i][cursor] = tmp # 바로 옆에 붙임
+
+    return board
+
+def right(board):
+    for i in range(N):
+        cursor = N - 1
+        for j in range(N - 1, -1, -1):
+
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[i][cursor] == 0:
+                    board[i][cursor] = tmp
+
+                elif board[i][cursor] == tmp:
+                    board[i][cursor] *= 2
+                    cursor -= 1
                 else:
-                    queue.append(board[r][c])
-            for i in range(len(queue)):
-                new_board[i][c] = queue[i]
+                    cursor -= 1
+                    board[i][cursor] = tmp
+    return board
 
-    elif direction == 1:  # 아래쪽 이동
-        for c in range(N):
-            queue, merged = [], [False] * N
-            for r in range(N-1, -1, -1):
-                if board[r][c] == 0:
-                    continue
-                if queue and queue[-1] == board[r][c] and not merged[len(queue)-1]:
-                    queue[-1] *= 2
-                    merged[len(queue)-1] = True
+def up(board):
+    for j in range(N):
+        cursor = 0
+        for i in range(N):
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[cursor][j] == 0:
+                    board[cursor][j] = tmp
+
+                elif board[cursor][j] == tmp:
+                    board[cursor][j] *= 2
+                    cursor += 1
+
                 else:
-                    queue.append(board[r][c])
-            for i in range(len(queue)):
-                new_board[N-1-i][c] = queue[i]
+                    cursor += 1
+                    board[cursor][j] = tmp
+    return board
 
-    elif direction == 2:  # 왼쪽 이동
-        for r in range(N):
-            queue, merged = [], [False] * N
-            for c in range(N):
-                if board[r][c] == 0:
-                    continue
-                if queue and queue[-1] == board[r][c] and not merged[len(queue)-1]:
-                    queue[-1] *= 2
-                    merged[len(queue)-1] = True
+def down(board):
+    for j in range(N):
+        cursor = N - 1
+        for i in range(N - 1, -1, -1):
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[cursor][j] == 0:
+                    board[cursor][j] = tmp
+
+                elif board[cursor][j] == tmp:
+                    board[cursor][j] *= 2
+                    cursor -= 1
+
                 else:
-                    queue.append(board[r][c])
-            for i in range(len(queue)):
-                new_board[r][i] = queue[i]
+                    cursor -= 1
+                    board[cursor][j] = tmp
+    return board
 
-    elif direction == 3:  # 오른쪽 이동
-        for r in range(N):
-            queue, merged = [], [False] * N
-            for c in range(N-1, -1, -1):
-                if board[r][c] == 0:
-                    continue
-                if queue and queue[-1] == board[r][c] and not merged[len(queue)-1]:
-                    queue[-1] *= 2
-                    merged[len(queue)-1] = True
-                else:
-                    queue.append(board[r][c])
-            for i in range(len(queue)):
-                new_board[r][N-1-i] = queue[i]
-
-    return new_board
-
-# DFS 탐색
-def dfs(board, depth):
-    global max_value
-    max_value = max(max_value, max(map(max, board)))  # 최대값 갱신
-
-    if depth == 5:
+def dfs(n, arr):
+    global ans
+    if n == 5:
+        for i in range(N):
+            for j in range(N):
+                if arr[i][j] > ans:
+                    ans = arr[i][j]
         return
 
-    for d in range(4):
-        new_board = move(copy.deepcopy(board), d)
-        if new_board != board:  # 이동이 발생했을 경우에만 재귀 호출
-            dfs(new_board, depth + 1)
+    for i in range(4):
+        copy_arr = copy.deepcopy(arr)
+        if i == 0:
+            dfs(n + 1, left(copy_arr))
+        elif i == 1:
+            dfs(n + 1, right(copy_arr))
+        elif i == 2:
+            dfs(n + 1, up(copy_arr))
+        else:
+            dfs(n + 1, down(copy_arr))
 
-# 최대값 저장 변수
-max_value = 0
-dfs(board, 0)
-
-# 정답 출력
-print(max_value)
+dfs(0, pan)
+print(ans)
