17b29f6d05b1fef9169fbb563b8b4baef4470c30
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2025-03-18 15:41:33 +0900

    [level 0] Title: 특이한 정렬, Time: 0.70 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,19 +1,32 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] numlist, int n) {        
-        Arrays.sort(numlist);
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = {};
         
-        for (int i = 0; i < numlist.length; i++) {
-            for (int j = 0; j < numlist.length; j++) {
-                if (Math.abs(numlist[i] - n) <= Math.abs(numlist[j] - n)) {
-                    int temp = numlist[i];
-                    numlist[i] = numlist[j];
-                    numlist[j] = temp;
-                }
-            }
+        List<Integer> list = new ArrayList<>();
+        for (int num: numlist) {
+            list.add(num);
         }
         
-        return numlist;
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                int diffA = Math.abs(a - n);
+                int diffB = Math.abs(b - n);
+                if (diffA != diffB) {
+                    return diffA - diffB;
+                } else {
+                    return b - a;
+                }
+            }
+        });
+        
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }
