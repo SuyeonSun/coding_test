f2fadcf1e514ca49dcfcb2de85fb85b6d8aa0d5c
선수연 <77823761+SuyeonSun@users.noreply.github.com>
2025-03-18 16:13:01 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 12372 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,59 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	private static int N;
-	private static int M;
-	private static int[][] arr;
-	private static int[][] dist;
-	private static final int[] dx = {-1, 1, 0, 0};
-	private static final int[] dy = {0, 0, -1, 1};
+    private static int N;
+    private static int M;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
-	public static void bfs(int i, int j) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] {i, j});
-		dist[i][j] = 0; // 첫 시작이기 때문에 거리는 0이다.
+    static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {0, 0, 1});
+        visited[0][0] = true;
 
-		while (!queue.isEmpty()) {
-			int[] poll = queue.poll();
-			int x = poll[0];
-			int y = poll[1];
-
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-				// 벽에 부딪히거나 범위를 넘어가는 경우
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-				// 길이 아니거나 이미 방문을 한 경우
-				if (arr[nx][ny] == 0 || dist[nx][ny] != -1) continue;
-
-				queue.add(new int[]{nx, ny});
-				dist[nx][ny] = dist[x][y] + 1;
-			}
-		}
-		System.out.println(dist[N-1][M-1] + 1);
-	}
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int c = poll[2];
+            if (x == N - 1 && y == M - 1) {
+                return c;
+            }
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                    continue;
+                }
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.add(new int[] {nx, ny, c+1});
+                }
+            }
+        }
+        return -1;
+    }
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		dist = new int[N][M];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
 
-		for (int n = 0; n < N; n++) {
-			String str = in.readLine();
-			for (int m = 0; m < M; m++) {
-				arr[n][m] = str.charAt(m) - '0';
-				dist[n][m] = -1;
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            String str = in.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }        
 
-		bfs(0, 0);
+        int answer = bfs();
+        System.out.println(answer);
 	}
 }
